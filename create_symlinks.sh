76ee49cc33c3610a0f5af3abed953fe09b5bf51c
Michael Loo Chin Moy <michael.david.lcm@gmail.com>
2015-04-13 16:14:21 +1000

    Added getopt command line parsing and also the override flag to copy over existing files

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -9,9 +9,56 @@
 # Constant Variables 
 ################################################################################
 
+SCRIPT=`basename $0`
 IGNORE_LIST=(".DS_Store" "." ".." ".gitignore" ".git")
-COPY_LIST=(".vim")
 PWD=`pwd`
+OVERRIDE_FILES=false
+NUMARGS=$#
+NOW=$(date +"%m_%d_%Y")
+
+################################################################################
+# Methods
+################################################################################
+
+# Print help message to STDOUT
+help_message() {
+  echo ""
+  log "Help documentation for ${SCRIPT}"
+  log "Basic usage: ${SCRIPT}"
+  log "The following switches are recognized."
+  log "-o  -- optional, override existing files"
+  log "-h  -- Displays this help message."\\n
+  log "Example: ${SCRIPT}"\\n
+  exit 1
+}
+
+################################################################################
+# Command line arguments
+################################################################################
+log () {
+  echo -e "${SCRIPT}_${NOW}: $@"
+}
+
+# Check the number of arguments. If none are passed, print help and exit.
+if [ $NUMARGS -eq 0 ]; then
+  help_message
+fi
+
+# Parse command line flags
+while getopts oh flag; do
+  case $flag in
+    o) # Set the OVERRIDE_FILES variable to true
+      OVERRIDE_FILES=true
+      ;;
+    h) # show help message
+      help_message
+      ;;
+    \?) # unrecognized option - show help message
+      log -e \\n"Option -${OPTARG} not allowed." \\n\\n
+      help_message
+      ;;
+  esac
+done
 
 ################################################################################
 # Start of script
@@ -33,36 +80,28 @@ for file in .*; do
   done
 
   if [[ "${ignore_file_flag}" = true ]]; then
-    echo Ignoring "${file}"
+    log Ignoring "${file}"
     continue
   fi
 
-  copy_file_flag=false
-  for copy_file in "${COPY_LIST[@]}"; do
-    if [[ "${copy_file}" = "${file}" ]]; then
-      copy_file_flag=true
-      break
-    fi
-  done
-
-  if [[ "${copy_file_flag}" = true ]]; then
-    echo Copying "${file}"
-    cp -r "${PWD}/${file}" "${HOME}/"
-    continue
+  if [[ -f "${HOME}/${file}" && "${OVERRIDE_FILES}" = true ]]; then
+      mv -f "${HOME}/${file}" /tmp/
   fi
 
   if [[ -L "${HOME}/${file}" ]]; then
     # Ignore if the file exists and is already a symlink 
     if [[ "${PWD}/${file}" = `readlink ${HOME}/${file}` ]]; then
-      echo "Symlink already exists for ${file}"
+      log "Symlink already exists for ${file}"
     else
-      echo "Symlink already exists but have a different link."
+      log "Symlink already exists but have a different link."
     fi
   elif [[ -f "${HOME}/${file}" ]]; then
-      echo "An existing file was found at ${HOME}/${file}"
+      log "An existing file was found at ${HOME}/${file}"
+  elif [[ -d "${HOME}/${file}" ]]; then
+      log "An existing folder was found at ${HOME}/${file}"
   else
     # Create a symlink for the remaining files
-    echo Creating symlink for "${file}"
+    log "Creating symlink for ${file}"
     ln -s "${PWD}/${file}" "${HOME}/${file}"
   fi
 done
